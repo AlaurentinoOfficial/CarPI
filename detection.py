42cc7b8d0ba8b7c4da2d19c25375fcb0e9c89626
Anderson Laurentino <alaurentino.br@gmail.com>
2017-12-31 14:40:57 -0300

    Add HoG circle detection

--- detection.py
+++ detection.py
@@ -3,12 +3,41 @@ import cv2
 import numpy as np
 import sys
 
+# -*- coding: UTF-8 -*-
+
+def detectCircles(frame):
+    # Create a gray mask
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Blur
+    gray = cv2.medianBlur(gray, 5)
+    
+    # HoG Circle detection
+    rows = gray.shape[0]
+    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, rows / 8,
+                               param1=100, param2=30,
+                               minRadius=1, maxRadius=30)   
+    
+    return circles
+
 # Camera
 cam = cv2.VideoCapture(0)
 
 while True:
     # Capture the next frame
     ret, frame = cam.read()
+
+    # Get the circles and show in frame
+    circles = detectCircles(frame)
+    if circles is not None:
+        print(len(circles[0, :]))
+        circles = np.uint16(np.around(circles))
+        for i in circles[0, :]:
+            center = (i[0], i[1])
+            # circle center
+            cv2.circle(frame, center, 1, (0, 100, 100), 3)
+            # circle outline
+            radius = i[2]
+            cv2.circle(frame, center, radius, (255, 0, 0), 3)
 
     # Show the frame in window
     cv2.imshow('Circle detection', frame)
