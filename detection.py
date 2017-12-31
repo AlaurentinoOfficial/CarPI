a611bf39ef9065f234d81df6b10e94c52df7d059
Anderson Laurentino <alaurentino.br@gmail.com>
2017-12-31 19:01:22 -0300

    Create a main file

--- detection.py
+++ detection.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-import sys
 
 # -*- coding: UTF-8 -*-
 
@@ -63,30 +62,3 @@ def controller(circles):
         return buff
     else:
         return -1
-
-# Camera
-cam = cv2.VideoCapture(0)
-
-while True:
-    # Capture the next frame
-    ret, frame = cam.read()
-    frame = cv2.flip(frame, 1)
-
-    # Get the circles and show in frame
-    circles = detectCircles(frame)
-    printCircles(frame, circles)
-    
-    # Get next command
-    print(controller(circles))
-
-    # Show the frame in window
-    cv2.imshow('Circle detection', frame)
-
-    # Exit keys
-    key = cv2.waitKey(30) & 0xFF
-    if key == 27 or key == ord('q'):
-        break
-
-# Release of the program
-cam.release()
-cv2.destroyAllWindows()
