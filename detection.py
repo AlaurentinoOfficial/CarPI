349b3e9b4f49280ddf3a741b8a303d8cf64cd3ea
Anderson Laurentino <alaurentino.br@gmail.com>
2018-01-02 05:28:22 -0300

    Create a simple line check

--- detection.py
+++ detection.py
@@ -7,13 +7,13 @@ def circles(frame):
     # Create a gray mask
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # Blur
-    gray = cv2.medianBlur(gray, 5)
+    gray = cv2.medianBlur(gray, 5, 5)
     
     # HoG Circle detection
     rows = gray.shape[0]
-    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, rows / 8,
+    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 27,
                                param1=100, param2=30,
-                               minRadius=1, maxRadius=30)
+                               minRadius=1, maxRadius=50)
     
     if circles is not None:
         circles = np.uint16(np.around(circles)) 
@@ -62,3 +62,46 @@ def controller(circles):
         return buff
     else:
         return 1
+
+def getVelocity(ctrl):
+    if ctrl is 0:
+        return (75, 75)
+    elif ctrl is 1:
+        return (50, -50)
+    else:
+        return (-50, 50)
+
+lower_black = np.array([30,30,30])
+upper_black = np.array([100,100,100])
+
+def line(frame, height, previous=None):
+    distance = None
+
+    crop = None
+    if previous is None:
+        crop = frame[(height-50):(height+50), 0:600]
+    else:
+        crop = frame[(height-5):(height+5), (previous[0]-25):(previous[0]+25)]
+
+    gray = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray,(5,5),0)
+
+    ret,thresh = cv2.threshold(blur,100,255,cv2.THRESH_BINARY_INV)
+    im2, contours,hierarchy = cv2.findContours(thresh.copy(), 1, cv2.CHAIN_APPROX_NONE)
+
+    if len(contours) > 0:
+        c = max(contours, key=cv2.contourArea)
+        M = cv2.moments(c)
+
+        if M['m00'] > 0:
+            center = (int(M['m10']/M['m00']), height)
+
+            cv2.circle(frame, center, 3, (0, 0, 255), 3)
+            cv2.drawContours(crop, contours, -1, (0,255,0), 1)
+
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.putText(frame,str(center[0] - 300),(center[0] + 10, center[1]), font, 0.8,(255,0,255), 1,cv2.LINE_AA)
+
+            distance = center[0] - 300
+    
+    return distance
--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import cv2
 import numpy as np
+import imutils
 import time
 import detection as dt
+import motors as motors
 
 def updateCamera(cam):
     ret, frame = cam.read()
-
+    frame = imutils.resize(frame, width=600)
     return frame
 
-# Camera
-cam = cv2.VideoCapture(0)
+def updateLineFollow(frame):
+    # Check line variation
+    pend = [
+        dt.line(frame, 400),
+        dt.line(frame, 300),
+        dt.line(frame, 200),
+        dt.line(frame, 100)
+    ]
 
-while True:
-    time.sleep(0.1)
+    print(pend)
 
-    # Capture the next frame
-    frame = updateCamera(cam)
-
+def updateRescue(frame):
     # Get the circles and show in frame
     circles = dt.circles(frame)
     dt.print(frame, circles)
@@ -26,8 +31,29 @@ while True:
     font = cv2.FONT_HERSHEY_SIMPLEX
     cv2.putText(frame,'CTRL: ' + str(ctrl),(10,25), font, 0.8,(0,0,0), 1,cv2.LINE_AA)
 
+    # Update the velocity
+    vel = dt.getVelocity(ctrl)
+    motors.setVelocity(vel)
+
+# Camera
+cam = cv2.VideoCapture(0)
+
+cv2.namedWindow('Circle detection')
+
+while True:
+    #time.sleep(0.1)
+
+    # Capture the next frame
+    frame = updateCamera(cam)
+
+    # LineFollow
+    updateLineFollow(frame)
+
+    # Rescue line
+    # updateRescue(frame)
+
     # Show the frame in window
-    cv2.imshow('Circle detection', frame)
+    cv2.imshow('Testing', frame)
 
     # Exit keys
     key = cv2.waitKey(30) & 0xFF
